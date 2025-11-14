d8a3a75e46940617098ca68d12a5086aa7862944
VanAnh555 <161809527+VanAnh555@users.noreply.github.com>
2025-11-14 11:15:28 +0700

    xoa quan ly dispatcher

--- server/src/main/java/com/logiflow/server/services/manager/dispatch/DispatchService.java
+++
@@ -1,7 +0,0 @@
-package com.logiflow.server.services.manager.dispatch;
-
-import com.logiflow.server.dtos.manager.dispatch.DispatchSummaryDto;
-
-public interface DispatchService {
-    DispatchSummaryDto getSummary();
-}
