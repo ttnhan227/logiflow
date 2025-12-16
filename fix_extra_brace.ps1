b5ac81025a2058eafe4d0c87d77b7950c543633d
Tran Trong Nhan <ttnhan227@gmail.com>
2025-12-16 17:01:51 +0700

    remove extra

--- fix_extra_brace.ps1
+++
@@ -1,7 +0,0 @@
-$file = 'd:\logiflow\client_mobile\lib\screens\driver\driver_trip_detail_screen.dart'
-$content = Get-Content $file -Raw
-$lines = $content -split "`n"
-if ($lines[-1] -eq '}' -and $lines[-2] -eq '}') {
-    $lines = $lines[0..($lines.Length-2)]
-}
-$lines -join "`n" | Set-Content $file -NoNewline
